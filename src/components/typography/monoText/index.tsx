1aee0cde715a2053ba446a0c1aed161e62e495ee
Mia <vottus@vott.us>
2021-01-08 10:43:32 +0100

    fix: monotext too dark

--- src/components/typography/monoText/index.tsx
+++ src/components/typography/monoText/index.tsx
@@ -1,5 +1,5 @@
 import tw from "twin.macro";
 
-const MonoText = tw.p`text-gray-400 text-xs font-thin font-mono max-w-100 m-0 mt-3`;
+const MonoText = tw.p`text-gray-300 text-xs font-normal font-mono max-w-100 m-0 mt-3`;
 
 export default MonoText;
